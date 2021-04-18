b4a52d4e18337f42241baf64a382e7b0662c6422
Oleg Stus <bakayrooleg@gmail.com>
2021-04-18 12:59:35 +0200

    update some code in html and css

--- js/main.js
+++ js/main.js
@@ -1,41 +1,34 @@
-$(document).ready(function() {
-		$('.bxslider').bxSlider({
-			controls: false,
-			randomStart: false,
-			auto: true
-		});
+$(document).ready(function () {
+	$('.bxslider').bxSlider({
+		controls: false,
+		randomStart: false,
+		auto: true
+	});
 
 	var owl = $("#owl-slider");
-			owl.owlCarousel({
-							itemsCustom : [
-									[0, 2],
-									[320, 2],
-									[450, 3],
-									[600, 4],
-									[700, 4],
-									[1000, 6]
-							],
-							autoplay:false,
-							autoplayTimeout:5000,
-							autoplayHoverPause:false,
-							navigation : false
-			});
+	owl.owlCarousel({
+		itemsCustom: [
+			[0, 2],
+			[320, 2],
+			[450, 3],
+			[600, 4],
+			[700, 4],
+			[1000, 6]
+		],
+		autoplay: false,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: false,
+		navigation: false
+	});
 
-		$('.comments-list').bxSlider({
-			pager: false
-		});
+	$('.comments-list').bxSlider({
+		pager: false
+	});
 
 
-		$("#header .nav-btn").on('click', function() {
-			if($(window).width() < 600){
-				$("#header .main-nav").toggleClass("main-nav-mobile");
-			}
-		});
-
-		$('.grid').masonry({
-			columnWidth: '.grid-sizer',
-			itemSelector: '.grid-item',
-			gutter: 10,
-			percentPosition: true
-		});
+	$(".header .nav-btn").on('click', function () {
+		if ($(window).width() < 600) {
+			$(".header .main-nav").toggleClass("main-nav-mobile");
+		}
+	});
 });
